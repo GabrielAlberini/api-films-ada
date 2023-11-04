2820b21ddccad3b7258f4be3ecd0918e925fc40a
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-03 21:46:55 -0300

    Creación de conexión del cliente

--- client.js
+++ client.js
@@ -1,1 +1,14 @@
-console.log("Hola desde el cliente");
+import net from "node:net";
+import dotenv from "dotenv";
+dotenv.config();
+
+const OPTIONS = {
+  port: process.env.PORT,
+  host: process.env.HOST,
+};
+
+const clientTCP = net.createConnection(OPTIONS);
+
+clientTCP.on("connect", () => {
+  console.log("Connected to server :D");
+});
