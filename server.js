4d06d1813aa77cdca00e3fc6e8c63098b3a0659a
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-03 21:11:57 -0300

    Creación del servidor TCP

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-console.log("Hola desde server");
-console.log("Hola de nuevo");
+import net from "node:net";
+
+const serverTCP = net.createServer();
+
+serverTCP.on("connection", () => {
+  console.log("Client connected :)");
+});
+
+serverTCP.listen(1234, () => {
+  console.log("Server UP");
+});
